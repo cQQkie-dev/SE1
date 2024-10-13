597b2924e5a04c5c745401a957c1eb7e4cf1e5f1
mkoch <matthias.koch@smail.inf.h-brs.de>
2024-10-13 16:28:05 +0200

    Exercise 2

--- src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
@@ -1,4 +1,37 @@
 package org.hbrs.se1.ws24.exercises.uebung2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Container {
+    private List<Member> members = new ArrayList<>();
+
+    public void addMember(Member member) throws ContainerException{
+        for(Member m: members){
+            if(m.getID().equals(member.getID())){
+                throw new ContainerException("Das Member-Objekt mit der ID " + member.getID() + " ist bereits vorhanden!");
+            }
+            members.add(member);
+        }
+    }
+
+    public void deleteMember(Integer id){
+        for(Member m: members){
+            if(m.getID().equals(id)){
+                members.remove(m);
+                return;
+            }
+        }
+        System.out.println("Fehler: Kein Member mit ID" + id + " gefunden.");
+        return;
+    }
+
+    public void dump(){
+        for(Member m: members){
+            System.out.println(m.toString());
+        }
+    }
+    public int size(){
+        return members.size();
+    }
 }
---
+++ test/org/hbrs/se1/ws24/tests/uebung2/ContainerTest.java
@@ -0,0 +1,8 @@
+package org.hbrs.se1.ws24.tests.uebung2;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContainerTest {
+
+}
